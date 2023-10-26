64235702f303caa0275c5a7b147746a4af12dd92
dinhson-nguyen <son.nd1701@gmail.com>
2023-10-26 18:33:43 +0700

    add triangle.h

---
+++ triangle.h
@@ -0,0 +1,31 @@
+//
+// Created by saurom on 10/26/2023.
+//
+//link https://leetcode.com/problems/triangle/
+#ifndef CODELEET_TRIANGLE_H
+#define CODELEET_TRIANGLE_H
+
+#endif //CODELEET_TRIANGLE_H
+
+#include "stdio.h"
+#include <iostream>
+#include <climits>
+#include "vector"
+
+using namespace std;
+class Solution {
+public:
+//    int min_sum = INT_MIN;
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int min_sum = INT_MAX;
+        for (int i=triangle.size()-1;i>=0 ;i--)
+        {
+            for (int j=0;i>=j ;j++) {
+
+            }
+        }
+
+
+    }
+};
